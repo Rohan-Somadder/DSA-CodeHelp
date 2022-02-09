d2c0520ee1c40cafa8c16119f80b4eb46091de24
Rohan Somadder <rohansomadder10@gmail.com>
2022-02-09 22:12:20 +0530

    Work : 09-02-22

--- 1. Basic/Valid_palindrome.cpp
+++ 1. Basic/Valid_palindrome.cpp
@@ -8,8 +8,38 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPalindrome(string s)
+{
+    int right = s.length(), left = 0;
+    while (left < right)
+    {
+        if (!isalpha(s[right]))
+            right--;
+        else if (!isalpha(s[left]))
+            left++;
+        else if (tolower(s[left]) != tolower(s[right]))
+            return false;
+        else
+        {
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+    if (isPalindrome(s))
+    {
+        cout << "The string is palindrome!";
+    }
+    else
+    {
+        cout << "The string is not palindrome!";
+    }
     return 0;
 }
