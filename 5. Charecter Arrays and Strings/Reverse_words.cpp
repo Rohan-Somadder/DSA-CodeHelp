424be9ba9f0a95788de3cc2edb1c743b66cb8889
Rohan Somadder <rohansomadder10@gmail.com>
2022-04-22 02:01:09 +0530

    work: 21-04-2022

---
+++ 5. Charecter Arrays and Strings/Reverse_words.cpp
@@ -0,0 +1,39 @@
+/*
+Given a string, you need to reverse the order of characters
+in each word within a sentence while still preserving whitespace
+and initial word order.
+
+Example -
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s)
+{
+    string ans, word;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            word.push_back(s[i]);
+        }
+        if (s[i] == ' ' && s[i + 1] != ' ')
+        {
+            ans = ans + word + " ";
+            word.clear();
+        }
+    }
+}
+
+int main()
+{
+    string s, ans;
+    cout << "Enter the string: ";
+    getline(cin, s);
+    ans = reverseWords(s);
+    cout << "String after : " << ans;
+    return 0;
+}
--- 5. Charecter Arrays and Strings/Reverse_words_in_string.cpp
+++ 5. Charecter Arrays and Strings/Reverse_words_in_string.cpp
@@ -8,7 +8,55 @@ Example 1 -
 Input: s = "the sky is blue"
 Output: "blue is sky the"
 
-Example 2 - 
+Example 2 -
 Input: "a b c"
 Output: "c b a"
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string trim(string s)
+{
+    if (s[0] == ' ')
+    {
+        s.pop_back();
+        reverse(s.begin(), s.end());
+        s.pop_back();
+        reverse(s.begin(), s.end());
+    }
+    else
+    {
+        s.pop_back();
+    }
+    return s;
+}
+string reverseWords(string s)
+{
+    string ans, word;
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (s[i] != ' ')
+        {
+            word.push_back(s[i]);
+        }
+        if (i == 0 || (s[i] == ' ' && s[i - 1] != ' '))
+        {
+            reverse(word.begin(), word.end());
+            ans = ans + word;
+            ans.push_back(' ');
+            word.clear();
+        }
+    }
+    return trim(ans);
+}
+
+int main()
+{
+    string s, ans;
+    cout << "Enter the string: ";
+    getline(cin, s);
+    ans = reverseWords(s);
+    cout << "String after : " << ans;
+    return 0;
+}
